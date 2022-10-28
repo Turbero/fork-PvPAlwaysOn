c856adc6385c1dfa6f990c2b852e60a2b9753235
AzumattDev <azumattgaming@gmail.com>
2022-10-28 15:39:07 -0400

    v1.0.2

--- PvPAlwaysOn.cs
+++ PvPAlwaysOn.cs
@@ -12,7 +12,7 @@ namespace PvPAlwaysOn;
 public class PvPAlwaysPlugin : BaseUnityPlugin
 {
     internal const string ModName = "PvPAlwaysOn";
-    internal const string ModVersion = "2.0.1";
+    internal const string ModVersion = "2.0.2";
     private const string ModGUID = "Azumatt.PvPAlwaysOn";
     public static string ConnectionError = "";
     private static string ConfigFileName = ModGUID + ".cfg";
